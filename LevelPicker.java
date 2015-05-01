2b14263b852b868a3de5141ddd4f4f1ff3c7eeac
Howard Dawson <hfdawson13@students.desu.edu>
2015-04-30 22:57:08 -0400

    level picker change

--- LevelPicker.java
+++ LevelPicker.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class LevelPicker extends World
+public class LevelPicker extends MainMenu
 {
     Easy easyButton = new Easy();
     Medium mediumButton = new Medium();
